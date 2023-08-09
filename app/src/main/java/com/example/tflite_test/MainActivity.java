a21a0275d32d9d6461b9296c5f01a0d3d9894d19
jamjamzo <jaeyoun92@gmail.com>
2023-08-09 17:19:52 +0900

    230809 RecordActivity 추가 및 버튼 생성

--- app/src/main/java/com/example/tflite_test/MainActivity.java
+++ app/src/main/java/com/example/tflite_test/MainActivity.java
@@ -13,43 +13,41 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button actBtn = findViewById(R.id.activity_launch_Btn);
-    actBtn.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            Intent i = new Intent(MainActivity.this, MyActivity.class);
-            startActivity(i);
-        }
-    });
 
-    Button drawBtn = findViewById(R.id.drawBtn);
-        drawBtn.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button drawBtn = findViewById(R.id.drawBtn);
+        drawBtn.setOnClickListener(view -> {
             Intent i = new Intent(MainActivity.this, DrawActivity.class);
             startActivity(i);
-        }
-    });
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
-//
-//        Button drawBtn = findViewById(R.id.drawBtn);
-//        drawBtn.setOnClickListener(view -> {
-//            Intent i = new Intent(MainActivity.this, DrawActivity.class);
-//            startActivity(i);
-//        });
-//    }
+        });
 
-    Button recordBtn = findViewById(R.id.recordBtn);
-    recordBtn.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            Intent i = new Intent(MainActivity.this, RecordActivity.class);
-            startActivity(i);
-        }
-    });
+        Button actBtn = findViewById(R.id.activity_launch_Btn);
+        actBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, MyActivity.class);
+                startActivity(i);
+            }
+        });
+
+
+
+        Button recordBtn = findViewById(R.id.recordBtn);
+        recordBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, RecordActivity.class);
+                startActivity(i);
+            }
+        });
+
+    }
+
+
 
 
 }
